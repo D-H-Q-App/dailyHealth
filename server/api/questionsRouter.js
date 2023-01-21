9955e8be8b99c537ffb39aaa6803a3aed2a98d72
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-01-21 11:01:56 -0500

    made comment

--- server/api/questionsRouter.js
+++ server/api/questionsRouter.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { Question } = require("../db")
 
+//This is a change
+
 router.get("/", async (req, res, next) => {
   try {
     const questions = await Question.findAll();
