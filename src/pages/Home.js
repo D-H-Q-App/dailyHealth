660504eed2a103af02130ba4e53a72c11f00e49c
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-02-26 20:04:59 -0800

    Updated the MetobolicType, About Ayuveda page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,21 +13,20 @@ function HomePage() {
         </Typography>
       </Box>
 
-      <Typography variant="h4" color="secondary">
+      <Typography variant="h6" color="secondary">
         {" "}
         Here, similar to Ayurvedic Medicine, we believe food can be healing.
-        Below, we have a daily yes or no health quiz that helps give food suggestions
-        based on any imbalances you may or may not be experincing! Some of these questions may sound a little far fetched or wild to you but go with it, our bodies are weird sometime! If you're only experiencing one of the symptoms listed, please still answer yes. {" "}
+        Below, we have a daily yes or no health quiz that gives food suggestions
+        based on any imbalances you may or may not be experincing! Some of these questions may sound a little far fetched or wild to you, but go with it...our bodies are weird sometimes! If you're only experiencing one of the symptoms listed, please still answer yes. {" "}
       </Typography>
 
      <div>
       <Questions/>
      </div>
       <p>
-        Note we have studied nutrition and Ayurvedic medicine however we are not
+        Note we have studied nutrition and Ayurvedic medicine however we're not
         certified Ayurvedic or Western Doctors. These are suggestions and we are
-        not held liable. All these suggests are food related not herbal. If you
-        would like to see a trained Ayuvedic Doctor. I highly suggest contacting
+        not held liable. All these suggests are food related not herbal. If you.d like to see a trained Ayuvedic Doctor. I highly suggest contacting
         Kerala or searching for a doctor at NAMA.{" "}
       </p>
     </div>
